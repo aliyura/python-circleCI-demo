d28a90a5730e407dd8e9cc0227413b48a003053d
Rabiu Aliyu <net.rabiualiyu@gmail.com>
2021-02-13 07:17:15 +0100

    Initial Release

---
+++ main.py
@@ -0,0 +1,8 @@
+def calculate(input):
+    res = eval(input)
+    print(res)
+    return res
+
+
+if __name__ == '__main__':
+    calculate("5+5")
---
+++ test.py
@@ -0,0 +1,8 @@
+from main import calculate
+
+def testCalculator():
+    assert calculate("10+19") == 20
+    print("It worked")
+
+
+testCalculator()
